991614f2445a670912fbca43824a49ca8bc7f207
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-01-16 15:13:08 -0300

    tornando responsivo a area de prioridades

--- src/components/ShortTask/styles.js
+++ src/components/ShortTask/styles.js
@@ -3,18 +3,18 @@ import styled from 'styled-components';
 export const Container = styled.button`
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   color: ${({ theme }) => theme.COLORS.GRAY_100};
-  height: 44px;
+  height: 2.75rem;
   border: 0;
-  padding: 0 16px;
+  padding: 0 1rem;
   border-radius: 7px;
-  font-size: 14px;
+  font-size: 0.875rem;
   
   display: flex;
   align-items: center;
   gap: 7px;
 
   > svg {
-    font-size: 18px;
+    font-size: 1.125rem;
     color: ${({ theme }) => theme.COLORS.RED_200};
   }
 `;
